d02846e84d125a45853f22a0e9d2d36565021a1a
qaz6758 <143369984+qaz6758@users.noreply.github.com>
2026-08-12 14:42:38 +0800

    删除了textlab引用

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from 'path'
+
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        index: resolve(process.cwd(), 'index.html'),
-        textLab: resolve(process.cwd(), 'text-lab.html')
-      }
-    }
-  },
 });
